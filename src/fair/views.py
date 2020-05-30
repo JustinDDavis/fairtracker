df70facca4dfa6264391a1e7304b011e528c8e08
Justin Davis <justindaniel92@gmail.com>
2020-05-30 17:23:19 -0500

    New fairs can be added for a user

--- src/fair/views.py
+++ src/fair/views.py
@@ -1,10 +1,29 @@
 
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
+from .models import Fair
+from .forms import FairForm
 
 def index(request):
-    return render(request, "all_fairs.html", {
-        "current_user_fairs": [{
-            'name':'Fair 1'
-        }]
-    })
+
+    if request.method == "POST":
+        form = FairForm(request.POST)
+        if form.is_valid():
+
+            form_object = form.save(commit=False)
+            
+            current_user = request.user
+            form_object.owner = current_user
+
+            print("ENd")
+            form_object.save()
+            return redirect("home")
+
+    all_fairs = Fair.objects.all()
+    fair_form = FairForm()
+    context = {
+        "current_user_fairs": all_fairs,
+        "form": fair_form
+    }
+
+    return render(request, "all_fairs.html", context)
