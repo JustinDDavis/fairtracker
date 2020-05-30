949bb462bdb7b0ebd1462a37eb9527e83bcf214c
Justin Davis <justindaniel92@gmail.com>
2020-05-30 11:28:38 -0500

    Social login works and can be assigned as staff.

--- src/homepage/views.py
+++ src/homepage/views.py
@@ -1,8 +1,21 @@
 from django.shortcuts import render, redirect
+import json
 
 def index(request):
     user = request.user
     if user.is_authenticated:
-        return render(request, 'secure.html')
+        user = request.user
+        auth0user = user.social_auth.get(provider='auth0')
+        userdata = {
+            'user_id': auth0user.uid,
+            'name': user.first_name,
+            'picture': auth0user.extra_data['picture'],
+            'email': auth0user.extra_data['email'],
+        }
+        return render(request, 'secure.html', {
+            'auth0User': auth0user,
+            'userdata': json.dumps(userdata, indent=4)
+        })
+
     else:
         return render(request, 'index.html')
