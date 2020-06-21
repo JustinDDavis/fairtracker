a47a0574129e8e121a236ae97b3a6a010b18fd82
Justin Davis <justindaniel92@gmail.com>
2020-06-20 20:38:18 -0500

    Minimal working participant form

---
+++ src/participant/views.py
@@ -0,0 +1,43 @@
+from django.shortcuts import render, redirect
+from django.contrib import messages
+
+from fair.models import Fair
+
+from .models import Participant
+from .forms import ParticipantForm
+
+
+def index(request):
+    if not request.user.is_authenticated:
+        return redirect("home")
+
+    if request.method == "POST":
+        form = ParticipantForm(request.POST)
+        if form.is_valid():
+            form_object = form.save(commit=False)
+
+            current_user = request.user
+
+            active_fair = Fair.objects.get(owner=current_user, active=True)
+
+            form_object.fair = active_fair
+
+            form_object.save()
+            messages.success(request, "Request was saved successfully")
+            return redirect("participant_home")
+
+        messages.error(request, form.errors)
+
+    all_participants = Participant.objects.all()
+
+    context = {
+        "current_user_participants": all_participants
+    }
+    return render(request, "all_participants.html", context)
+
+
+def delete(request, participant_id):
+    participant = Participant.objects.get(pk=participant_id)
+    participant.delete()
+    messages.success(request, "Participant was deleted  successfully")
+    return redirect('participant_home')
