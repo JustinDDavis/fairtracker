82405cf7ed8ab5eccd7fd2e922b3394ad6d840eb
Justin Davis <justindaniel92@gmail.com>
2020-06-28 15:34:04 -0500

    Catalog Items added to project

--- src/catalog/models.py
+++ src/catalog/models.py
@@ -5,4 +5,8 @@ from fair.models import Fair
 class Catalog(models.Model):
     name = models.CharField(max_length=60)
     description = models.CharField(max_length=60)
+    active = models.BooleanField(default=False)
     fair = models.ForeignKey(Fair, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"Catalog({self.name} | {self.description} | {self.active} | {self.fair})"
--- src/config/settings.py
+++ src/config/settings.py
@@ -47,7 +47,8 @@ INSTALLED_APPS = [
     'config',
     'fair',
     'participant',
-    'catalog'
+    'catalog',
+    'catalog_item'
 ]
 
 MIDDLEWARE = [
