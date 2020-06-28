1f4ddb17b1485ccb39ed86c51de44df125e6839d
Justin Davis <justindaniel92@gmail.com>
2020-06-27 20:56:42 -0500

    Fields updated and current fair name available

--- src/catalog/views.py
+++ src/catalog/views.py
@@ -1,5 +1,16 @@
 from django.shortcuts import render
 
-# Create your views here.
+from fair.models import Fair
+
+
 def index(request):
-    return render(request, "catalog.html")
+    fair_name = get_current_fair_name(request.user)
+    context = {
+        "current_fair_name": fair_name
+    }
+    return render(request, "catalog.html", context)
+
+
+def get_current_fair_name(current_user):
+    active_fair = Fair.objects.get(owner=current_user, active=True)
+    return active_fair.name
