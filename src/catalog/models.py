7a242a1b7d1d6716c0e744964eb7fda8afc268cb
Justin Davis <justindaniel92@gmail.com>
2020-06-27 21:31:33 -0500

    Catalog successfully post

--- src/catalog/models.py
+++ src/catalog/models.py
@@ -1,3 +1,8 @@
 from django.db import models
+from fair.models import Fair
 
-# Create your models here.
+
+class Catalog(models.Model):
+    name = models.CharField(max_length=60)
+    description = models.CharField(max_length=60)
+    fair = models.ForeignKey(Fair, on_delete=models.CASCADE)
