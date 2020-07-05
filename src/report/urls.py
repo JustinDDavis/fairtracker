fe9ca5c31b436efe2abf45718383947834010652
Justin Davis <justindaniel92@gmail.com>
2020-07-05 12:52:20 -0500

    Adding reporting and a print out page

--- src/report/urls.py
+++ src/report/urls.py
@@ -16,18 +16,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
-urlpatterns = [
-    path('', include('homepage.urls')),  # Landing page / Sign-in links
-    path('fair/', include('fair.urls')),  # App to handle creation / Edit of Apps
-    path('participant/', include('participant.urls')),  #
-    path('catalog/', include('catalog.urls')),  #
-    path('catalog-item/', include('catalog_item.urls')),  #
-    path('entry/', include('entry.urls')),  #
-    path('prize/', include('prize.urls')),  #
-    path('judge-sheet/', include('judge_sheet.urls')),  #
-    path('report/', include('report.urls')),  #
-    path('admin/', admin.site.urls),  # Admin Portal
-    path('', include('social_django.urls')),  # Auth0 Handlers /complete/auth0
-    path('', include('auth0login.urls')),  # /login  and /logout
-]
+from report import views
 
+urlpatterns = [
+    path('', views.index, name='report_home'),
+    path('print/', views.printer, name='report_printout'),
+]
