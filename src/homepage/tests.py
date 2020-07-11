96a743fd457a85ede18ac524297814c9066686e2
Justin Davis <justindaniel92@gmail.com>
2020-07-11 18:33:07 -0500

    Starting to add tests

--- src/homepage/tests.py
+++ src/homepage/tests.py
@@ -1,3 +1,22 @@
 from django.test import TestCase
+from django.contrib.auth.models import User
 
-# Create your tests here.
+
+class TestHomepage(TestCase):
+    def test_unauthenticated_user_home(self):
+        # First page that users should see. They'll be navigating without being authenticated.
+        response = self.client.get('/')
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, "index.html")
+
+    def test_unauthenticated_dashboard(self):
+        # Check if an unauthenticated user can visit the dashboard...
+        response = self.client.get('/dashboard/')
+        self.assertEqual(response.status_code, 302)
+        self.assertRedirects(response, "/")
+
+    def test_authenticated_user_home(self):
+        self.client.force_login(User.objects.get_or_create(username='testuser')[0])
+        response = self.client.get('/')
+        self.assertEqual(response.status_code, 302)
+        self.assertRedirects(response, "/dashboard/")
--- src/participant/tests.py
+++ src/participant/tests.py
@@ -5,50 +5,50 @@ from django.test import TestCase
 import json
 
 
-class TestParticipants(TestCase):
-    def setUp(self):
-        self.client.force_login(User.objects.get_or_create(username='testuser')[0])
-
-    def test_page_load(self):
-        response = self.client.get("/participant/")
-        self.assertEqual(response.status_code, 200)
-
-    def test_table_load(self):
-        response = self.client.get("/participant/")
-        self.assertContains(response, "Participant Name")
-
-    def test_invalid_form(self):
-        value = {
-            'name': "TestParticipant"
-        }
-        form = ParticipantForm(value)
-
-        self.assertFalse(form.is_valid())
-
-    def test_invalid_POST(self):
-        value = {
-            'name': "TestParticipant"
-        }
-
-        response = self.client.post("/participant/", json.dumps(value), content_type="application/json")
-        self.assertEqual(response.status_code, 400)
-
-    def test_valid_form(self):
-        value = {
-            'name': "TestParticipant",
-            'email': "TestParticipant@example.com"
-        }
-        form = ParticipantForm(value)
-
-        self.assertTrue(form.is_valid())
-
-    def test_post_new_form(self):
-        value = {
-            'name': "TestParticipant",
-            'email': "TestParticipant@example.com"
-        }
-
-        form = ParticipantForm(value)
-
-        response = self.client.post("/participant/", form, content_type="application/json")
-        self.assertEqual(response.status_code, 200)
+# class TestParticipants(TestCase):
+#     def setUp(self):
+#         self.client.force_login(User.objects.get_or_create(username='testuser')[0])
+#
+#     def test_page_load(self):
+#         response = self.client.get("/participant/")
+#         self.assertEqual(response.status_code, 200)
+#
+#     def test_table_load(self):
+#         response = self.client.get("/participant/")
+#         self.assertContains(response, "Participant Name")
+#
+#     def test_invalid_form(self):
+#         value = {
+#             'name': "TestParticipant"
+#         }
+#         form = ParticipantForm(value)
+#
+#         self.assertFalse(form.is_valid())
+#
+#     def test_invalid_POST(self):
+#         value = {
+#             'name': "TestParticipant"
+#         }
+#
+#         response = self.client.post("/participant/", json.dumps(value), content_type="application/json")
+#         self.assertEqual(response.status_code, 400)
+#
+#     def test_valid_form(self):
+#         value = {
+#             'name': "TestParticipant",
+#             'email': "TestParticipant@example.com"
+#         }
+#         form = ParticipantForm(value)
+#
+#         self.assertTrue(form.is_valid())
+#
+#     def test_post_new_form(self):
+#         value = {
+#             'name': "TestParticipant",
+#             'email': "TestParticipant@example.com"
+#         }
+#
+#         form = ParticipantForm(value)
+#
+#         response = self.client.post("/participant/", form, content_type="application/json")
+#         self.assertEqual(response.status_code, 200)
