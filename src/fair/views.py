845ac2c2f6f2b2a95862aaed75b665894408919e
Justin Davis <justindaniel92@gmail.com>
2020-07-17 22:37:06 -0500

    Index page test started

--- src/fair/views.py
+++ src/fair/views.py
@@ -17,7 +17,7 @@ def index(request):
         if form.is_valid():
 
             form_object = form.save(commit=False)
-            
+
             current_user = request.user
             form_object.owner = current_user
 
@@ -38,62 +38,60 @@ def index(request):
     else:
         all_fairs = all_fairs.order_by(Lower("name"))
 
-    # fair_form = FairForm()
     context = {
         "current_user_fairs": all_fairs,
-        # "form": fair_form
     }
 
-    return render(request, "all_fairs.html", context)
+    return render(request, "test.html", context)
 
-def activate(request, fair_id):
-
-    # TODO: COME BACK AND MAKE SURE THE CURRENT USER IS AN OWNER OF THIS FAIR
-    fair = Fair.objects.get(pk=fair_id)
-
-    print(fair.owner)
-    print(request.user)
-    if fair.owner != request.user:
-        print("Fair is not owned by this person")
-        return redirect('fair_home')
-
-    fair.active = not fair.active
-    fair.save()
-
-    # Set all others to False active
-    Fair.objects.filter(owner_id=request.user).exclude(pk=fair_id).update(active=False)
-
-    return redirect('fair_home')
-
-
-def edit(request, fair_id):
-    fair = Fair.objects.get(pk=fair_id)
-
-    if fair.owner != request.user:
-        print("Edit - Fair is not owned by this person")
-        return redirect('fair_home')
-
-    if request.method == "POST":
-        form = FairForm(request.POST, instance=fair)
-        if form.is_valid():
-            form.save()
-            messages.success(request, "Fair Updated Successfully")
-            return redirect("fair_home")
-        messages.error(request, form.errors)
-        return redirect("fair_edit")
-    else:
-        context = {
-            "fair": fair
-        }
-        return render(request, "edit_fair.html", context)
-
-
-def delete(request, fair_id):
-    fair = Fair.objects.get(pk=fair_id)
-    if fair.owner != request.user:
-        print("Delete - Fair is not owned by this person")
-        return redirect('fair_home')
-
-    fair.delete()
-    messages.success(request, "Fair was deleted  successfully")
-    return redirect('fair_home')
+# def activate(request, fair_id):
+#
+#     # TODO: COME BACK AND MAKE SURE THE CURRENT USER IS AN OWNER OF THIS FAIR
+#     fair = Fair.objects.get(pk=fair_id)
+#
+#     print(fair.owner)
+#     print(request.user)
+#     if fair.owner != request.user:
+#         print("Fair is not owned by this person")
+#         return redirect('fair_home')
+#
+#     fair.active = not fair.active
+#     fair.save()
+#
+#     # Set all others to False active
+#     Fair.objects.filter(owner_id=request.user).exclude(pk=fair_id).update(active=False)
+#
+#     return redirect('fair_home')
+#
+#
+# def edit(request, fair_id):
+#     fair = Fair.objects.get(pk=fair_id)
+#
+#     if fair.owner != request.user:
+#         print("Edit - Fair is not owned by this person")
+#         return redirect('fair_home')
+#
+#     if request.method == "POST":
+#         form = FairForm(request.POST, instance=fair)
+#         if form.is_valid():
+#             form.save()
+#             messages.success(request, "Fair Updated Successfully")
+#             return redirect("fair_home")
+#         messages.error(request, form.errors)
+#         return redirect("fair_edit")
+#     else:
+#         context = {
+#             "fair": fair
+#         }
+#         return render(request, "edit_fair.html", context)
+#
+#
+# def delete(request, fair_id):
+#     fair = Fair.objects.get(pk=fair_id)
+#     if fair.owner != request.user:
+#         print("Delete - Fair is not owned by this person")
+#         return redirect('fair_home')
+#
+#     fair.delete()
+#     messages.success(request, "Fair was deleted  successfully")
+#     return redirect('fair_home')
