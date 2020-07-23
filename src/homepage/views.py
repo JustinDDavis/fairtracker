c3a022676b414d846a102d1216b49676ce7e8259
Justin Davis <justindaniel92@gmail.com>
2020-07-22 21:04:14 -0500

    Adding basic metrics on homepage

--- src/homepage/views.py
+++ src/homepage/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render, redirect
 from fair.models import Fair
-
+from participant.models import Participant
+from catalog.models import Catalog
+from entry.models import Entry
+from prize.models import Prize
+from judge_sheet.models import JudgeSheet
 
 def index(request):
     if request.user.is_authenticated:
@@ -16,7 +20,20 @@ def user_homepage(request):
         return redirect('home')
 
     all_fair_items = Fair.objects.filter(owner=request.user)
+
+    active_fair = Fair.objects.get(owner=request.user, active=True)
+
+    participant_count = len(Participant.objects.filter(fair=active_fair))
+    catalog = Catalog.objects.get(fair=active_fair, active=True)
+    entry_count = len(Entry.objects.filter(catalog_item__catalog=catalog))
+
+    prize_money_awarded = 0
+    for judge_sheet in JudgeSheet.objects.filter(catalog_item__catalog=catalog):
+        prize_money_awarded += int(judge_sheet.prize.amount)
     
     return render(request, 'user_homepage.html', {
-        'fairs': all_fair_items
+        'fairs': all_fair_items,
+        'participant_count': participant_count,
+        'entry_count': entry_count,
+        'prize_money_awarded': prize_money_awarded
     })
